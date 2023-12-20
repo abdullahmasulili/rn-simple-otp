037def2436ffb490dd2a5184bf25b93e009f1218
abdullahmasulili <abdullah.masulili@gmail.com>
2023-12-20 15:33:06 +0800

    build(routes): init routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { Login, Otp, Register } from "~screens";
+
 const Stack = createNativeStackNavigator();
 
 export default function index() {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false, gestureEnabled: false }}
+      initialRouteName="Register"
     >
-      <Stack.Screen name="Login" />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Otp" component={Otp} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 }
